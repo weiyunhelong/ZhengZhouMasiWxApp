5a19ef4f8abc06715ce8f5f20492baedc9ed64b6
YunlongZhang <yunlong.zhang@asaplus.com.cn>
2021-11-26 14:40:07 +0800

    'code'

--- pages/jiyin/detail.js
+++ pages/jiyin/detail.js
@@ -1,7 +1,4 @@
-// pages/jiyin/index.js
-var requestUrl = getApp().globalData.requestUrl;
-var WxRequest = require('../../utils/WxRequest.js');
-
+// pages/jiyin/detail.js
 Page({
 
   /**
@@ -17,11 +14,7 @@ Page({
   onLoad: function (options) {
 
   },
-  goDetail(e){
-    wx.navigateTo({
-      url: '../jiyin/detail',
-    })
-  },
+
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -33,26 +26,9 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that = this;
-    //获取菜单的列表数据
-    that.setTabbarlist();
-  },
-  setTabbarlist: function () { //获取菜单的列表数据
-    var that = this;
 
-    //设置选中的tabbar
-    if (typeof that.getTabBar === 'function' && that.getTabBar()) {
-      for (var i = 0; i < getApp().globalData.tabbar.length; i++) {
-        if (getApp().globalData.tabbar[i].pagePath.indexOf("/jiyin/index") > 0) {
-          that.getTabBar().setData({
-            selected: i,
-            showTabBar: true,
-            list: getApp().globalData.tabbar
-          })
-        }
-      }
-    }
   },
+
   /**
    * 生命周期函数--监听页面隐藏
    */
