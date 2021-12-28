34dc2e51e810d39abdc1f005bb84481685de9a56
YunlongZhang <yunlong.zhang@asaplus.com.cn>
2021-12-28 15:17:32 +0800

    'code'

--- pages/test/result.js
+++ pages/test/result.js
@@ -1,6 +1,9 @@
 // pages/test/result.js
 var requestUrl = getApp().globalData.requestUrl;
 var WxRequest = require('../../utils/WxRequest.js');
+var interval;
+var varName;
+var ctx = wx.createCanvasContext('canvasArcCir');
 
 Page({
 
@@ -40,9 +43,57 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    var that = this;
+
+    //创建外环
+    var cxt_arc = wx.createCanvasContext('canvasCircle');
+    cxt_arc.setLineWidth(10);
+    cxt_arc.setStrokeStyle('#FCDAD9');
+    cxt_arc.setLineCap('round');
+    cxt_arc.beginPath();
+    cxt_arc.arc(100, 100, 95, 0, 2 * Math.PI, false);
+    cxt_arc.stroke();
+    cxt_arc.draw();
+
+    //创建并返回绘图上下文context对象。
+    that.drawCircle();
 
   },
+  drawCircle: function () {
+    clearInterval(varName);
 
+    //创建选中环
+    function drawArc(s, e) {
+      ctx.setFillStyle('white');
+      ctx.clearRect(0, 0, 200, 200);
+      ctx.draw();
+      var x = 100,
+        y = 100,
+        radius = 95;
+      ctx.setLineWidth(10);
+      ctx.setStrokeStyle('#F24540');
+      ctx.setLineCap('round');
+      ctx.beginPath();
+      ctx.arc(x, y, radius, s, e, false);
+      ctx.stroke()
+      ctx.draw()
+    }
+    var step = 1,
+      startAngle = 1.5 * Math.PI,
+      endAngle = 0;
+    var animation_interval = 1000,
+      n = 2;
+    var animation = function () {
+      if (step <= n) {
+        endAngle = step * parseFloat(27 / 100) * 2 * Math.PI / n + 1.5 * Math.PI;
+        drawArc(startAngle, endAngle);
+        step++;
+      } else {
+        clearInterval(varName);
+      }
+    };
+    varName = setInterval(animation, animation_interval);
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
