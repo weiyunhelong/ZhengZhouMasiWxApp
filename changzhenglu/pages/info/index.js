b58c0aa57d69d25cbd7094944273c7482d7c1751
屠刀下的小羊 <2556741824@qq.com>
2022-03-28 16:13:26 +0800

    code

--- changzhenglu/pages/info/index.js
+++ changzhenglu/pages/info/index.js
@@ -1,11 +1,17 @@
 // changzhenglu/pages/info/index.js
+var requestUrl = getApp().globalData.requestUrl;
+var WxRequest = require('../../../utils/WxRequest.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    swipers: ['https://zhengzhousizheng.oss-cn-beijing.aliyuncs.com/bannerimgv/%E5%AF%BB%E6%89%BE%E7%BA%A2%E8%89%B2%E7%B2%BE%E7%A5%9E.png', 'https://zhengzhousizheng.oss-cn-beijing.aliyuncs.com/bannerimgv/%E7%BA%A2%E8%89%B2%E6%97%B6%E6%97%B6%E8%AF%BB.png', 'https://zhengzhousizheng.oss-cn-beijing.aliyuncs.com/bannerimgv/%E5%AF%BB%E6%89%BE%E7%BA%A2%E8%89%B2%E7%B2%BE%E7%A5%9E.png'],
+    content: '<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;延安革命纪念馆位于陕西省延安市宝塔区西北延河东岸，距城1公里处，为国家AAAA级景区。整个纪念馆外观朴素大方，结构紧凑，高大宏伟，具有传统的民族风格。 </p><br/><br/>\
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;延安革命纪念馆始建于1950年1月，原馆址在南关交际处，是中华人民共和国成立后最早建立的革命纪念馆之一。1954年迁往杨家岭原中共中央机关旧址，定名为“延安博物馆”。 </p>\
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1955年迁至城内凤凰山麓革命旧址院内，改名为“延安革命纪念馆”，1973年6月迁往王家坪现址。</p>'
   },
 
   /**
@@ -14,14 +20,32 @@ Page({
   onLoad: function (options) {
 
   },
-
+  ImgLoad(e) { //获取图的高度
+    var that = this;
+    // 取一屏的，第一屏自适应然后按照第一屏的高度
+    var ratio = 0;
+    var $width = e.detail.width, //获取图片真实宽度
+      $height = e.detail.height,
+      ratio = $width / $height; //图片的真实宽高比例
+    var viewWidth = 750, //设置图片显示宽度，左右留有16rpx边距
+      viewHeight = viewWidth / ratio; //计算的高度值
+    that.setData({
+      imgh: viewHeight
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
 
   },
-
+  goVROpt() { //跳转到VR部分
+    var that = this;
+    var url = "https://www.baidu.com";
+    wx.navigateTo({
+      url: '../webview/index?url=' + url
+    })
+  },
   /**
    * 生命周期函数--监听页面显示
    */
--- changzhenglu/pages/line/index.js
+++ changzhenglu/pages/line/index.js
@@ -1,11 +1,15 @@
 // changzhenglu/pages/line/index.js
+var requestUrl = getApp().globalData.requestUrl;
+var WxRequest = require('../../../utils/WxRequest.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    showMask: false,
+    showMaskAni: false,
   },
 
   /**
@@ -22,7 +26,29 @@ Page({
       },
     })
   },
-
+  viewOpt(e) { //点击浏览
+    var that = this;
+    that.setData({
+      showMask: true,
+      showMaskAni: true,
+      dataobj: {
+        id:1
+      },
+    })
+  },
+  goWebOpt() { //进入参观
+    var that = this;
+    var dataobj = that.data.dataobj;
+    wx.navigateTo({
+      url: '../info/index?id=' + dataobj.id,
+      complete:function(){
+        that.setData({
+          showMaskAni: false,
+          showMask: false,
+        })
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
